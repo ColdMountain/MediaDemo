d41613e2ecd33c0b0f2acd8ef27964fd03831b8d
ColdMountain <609452414qq.com>
2022-12-29 10:48:03 +0800

    删除测试用例

--- MediaDemo/ViewController.mm
+++ MediaDemo/ViewController.mm
@@ -14,12 +14,6 @@
 @property (nonatomic, strong) CMAuidoPlayer_PCM  *audioPlayer;
 @property (nonatomic, strong) CMAudioSession_PCM *audioSession;
 
-@property (nonatomic, strong) CMAudioSession_PCM *audioSession1;
-@property (nonatomic, strong) CMAudioSession_PCM *audioSession2;
-@property (nonatomic, strong) CMAudioSession_PCM *audioSession3;
-@property (nonatomic, strong) CMAudioSession_PCM *audioSession4;
-@property (nonatomic, strong) CMAudioSession_PCM *audioSession5;
-
 @property (weak, nonatomic) IBOutlet UIButton *button;
 @property (weak, nonatomic) IBOutlet UIButton *echoButton;
 
@@ -42,25 +36,16 @@
     
     self.echoButton.selected = YES;
     [self.echoButton setBackgroundColor:[UIColor redColor]];
-    
-    self.audioSession1 = [[CMAudioSession_PCM alloc]initAudioUnitWithSampleRate:CMAudioPCMSampleRate_Defalut];
-    self.audioSession1.delegate = self;
-    
-    self.audioSession2 = [[CMAudioSession_PCM alloc]initAudioUnitWithSampleRate:CMAudioPCMSampleRate_32000Hz];
-    self.audioSession2.delegate = self;
-    
-    self.audioSession3 = [[CMAudioSession_PCM alloc]initAudioUnitWithSampleRate:CMAudioPCMSampleRate_44100Hz];
-    self.audioSession3.delegate = self;
 }
 
 - (void)cm_audioUnitBackPCM:(NSData*)audioData selfClass:(CMAudioSession_PCM*)selfClass{
-    if (selfClass == self.audioSession1) {
-        NSLog(@"CMAudioSession_PCM | cm_audioUnitBackPCM: 1、%p", selfClass);
-    }else if (selfClass == self.audioSession2) {
-        NSLog(@"CMAudioSession_PCM | cm_audioUnitBackPCM: 2、%p", selfClass);
-    }else if (selfClass == self.audioSession3) {
-        NSLog(@"CMAudioSession_PCM | cm_audioUnitBackPCM: 3、%p", selfClass);
-    }
+//    if (selfClass == self.audioSession1) {
+//        NSLog(@"CMAudioSession_PCM | cm_audioUnitBackPCM: 1、%p", selfClass);
+//    }else if (selfClass == self.audioSession2) {
+//        NSLog(@"CMAudioSession_PCM | cm_audioUnitBackPCM: 2、%p", selfClass);
+//    }else if (selfClass == self.audioSession3) {
+//        NSLog(@"CMAudioSession_PCM | cm_audioUnitBackPCM: 3、%p", selfClass);
+//    }
 //    [self.auidoHandle writeData:audioData];
 //    dispatch_async(dispatch_get_main_queue(), ^{
 //        [self.auidoHandle writeData:audioData];
@@ -68,54 +53,14 @@
 //    });
 }
 - (IBAction)echoAction:(UIButton*)sender {
-//    sender.selected = !sender.selected;
-//    if (sender.selected) {
-//        [self.audioSession cm_startEchoAudio:0];
-//        [self.echoButton setBackgroundColor:[UIColor redColor]];
-//    }else {
-//        [self.audioSession cm_startEchoAudio:1];
-//        [self.echoButton setBackgroundColor:[UIColor clearColor]];
-//    }
-    if (self.num == -1) {
-        [self.audioSession1 cm_startAudioUnitRecorder];
-        [self.audioSession2 cm_stopAudioUnitRecorder];
-//        [self.audioSession3 cm_stopAudioUnitRecorder];
-//        [self.audioSession4 cm_stopAudioUnitRecorder];
-//        [self.audioSession5 cm_stopAudioUnitRecorder];
-        self.num = 0;
-    }else if (self.num == 0){
-        [self.audioSession1 cm_stopAudioUnitRecorder];
-//        [self.audioSession3 cm_stopAudioUnitRecorder];
-        
-        [self.audioSession2 cm_startAudioUnitRecorder];
-//        [self.audioSession4 cm_stopAudioUnitRecorder];
-//        [self.audioSession5 cm_stopAudioUnitRecorder];
-        self.num = -1;
+    sender.selected = !sender.selected;
+    if (sender.selected) {
+        [self.audioSession cm_startEchoAudio:0];
+        [self.echoButton setBackgroundColor:[UIColor redColor]];
+    }else {
+        [self.audioSession cm_startEchoAudio:1];
+        [self.echoButton setBackgroundColor:[UIColor clearColor]];
     }
-    
-//    else if (self.num == 1){
-//        [self.audioSession1 cm_stopAudioUnitRecorder];
-//        [self.audioSession2 cm_stopAudioUnitRecorder];
-//        [self.audioSession3 cm_startAudioUnitRecorder];
-////        [self.audioSession4 cm_stopAudioUnitRecorder];
-////        [self.audioSession5 cm_stopAudioUnitRecorder];
-//        self.num = -1;
-//    }
-//    else if (self.num == 2){
-//        [self.audioSession1 cm_stopAudioUnitRecorder];
-//        [self.audioSession2 cm_stopAudioUnitRecorder];
-//        [self.audioSession3 cm_stopAudioUnitRecorder];
-////        [self.audioSession4 cm_startAudioUnitRecorder];
-////        [self.audioSession5 cm_stopAudioUnitRecorder];
-//        self.num = 3;
-//    }else if (self.num == 3){
-//        [self.audioSession1 cm_stopAudioUnitRecorder];
-//        [self.audioSession2 cm_stopAudioUnitRecorder];
-//        [self.audioSession3 cm_stopAudioUnitRecorder];
-////        [self.audioSession4 cm_stopAudioUnitRecorder];
-////        [self.audioSession5 cm_startAudioUnitRecorder];
-//        self.num = -1;
-//    }
 }
 
 - (IBAction)receiverAndSpeaker:(UIButton*)sender {
